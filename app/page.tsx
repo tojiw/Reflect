6948e55bb26a080ee49bc3706451d1749a605643
Waqas Faraz <farazwaqas@gmail.com>
2024-07-04 19:19:55 +0530

    added toolbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import FontButton from "@/components/FontButton";
 import Sidebar from "@/components/Sidebar";
+import Buttons from "@/components/ToolBar";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useRef } from "react";
 
 export default function Home() {
@@ -25,15 +27,25 @@ export default function Home() {
         <Sidebar />
       </div>
       <div className="flex flex-grow flex-col relative">
-        <div className="overflow-y-auto h-[70vh] flex rounded-lg m-5 no-sc">
+        <div className="overflow-y-auto h-[70vh] flex rounded-lg m-5">
           <textarea
             name="type"
             id=""
             placeholder="Start Reflecting..."
-            className="resize-none w-full  p-5 bg-[#121212] border border-[#5B1F00] outline-none rounded-lg text-white font-DM_sans tracking-tight placeholder:text-[#686868] placeholder:font-Instrument-Serif placeholder:text-xl placeholder:tracking-normal no-scrollbar"
+            className="resize-none w-full  p-5 bg-[#121212]  border-[#5B1F00] outline-none rounded-lg text-white font-DM_sans tracking-tight placeholder:text-[#686868] placeholder:font-Instrument-Serif placeholder:text-xl placeholder:tracking-normal no-scrollbar"
           ></textarea>
           <div className="flex sitcky justify-center absolute bottom-20 left-0 right-0">
-            <div className=" w-[450px] h-[50px] bg-[#171717] rounded-sm sticky "></div>
+            <div className=" w-[490px]  bg-[#171717] rounded-[4px] sticky flex flex-row gap-5 justify-center items-center py-3 max-sm:hidden ">
+              {Buttons.map((button,index)=>{
+                return(
+                  <div key={index} className="flex flex-row items-center ">
+                    <button onClick={button.onClick} className="" >
+                      <button.src size={40}  className=" text-[#EA580C] hover:text-[black] bg-[#1B1B1B] p-2 rounded-lg hover:bg-[#EA580C] transition-all duration-150" />
+                    </button>
+                  </div>
+                )
+              })}
+            </div>
           </div>
         </div>
       </div>
---
+++ components/ToolBar.tsx
@@ -0,0 +1,62 @@
+import { Camera,List,Bold,Underline, Italic,CaseUpperIcon,AudioLines,TagsIcon, LucideIcon } from "lucide-react";
+
+interface ButtonType{
+label:string,
+onClick:()=>void,
+src:LucideIcon,
+path:string
+}
+
+const Buttons: ButtonType[] = [
+  {
+    label:"Camera",
+    onClick:()=>console.log("Camera clicked"),
+    src:Camera,
+    path:"/"
+  },
+  {
+    label:"List",
+    onClick:()=>console.log("List clicked"),
+    src:List,
+    path:"/"
+
+  },
+  {
+    label:"Bold",
+    onClick:()=>console.log("Bold clicked"),
+    src:Bold,
+    path:"/"	
+  },
+  {
+    label:"Underline",
+    onClick:()=>console.log("Underline clicked"),
+    src:Underline,
+    path:"/"
+  },
+  {
+    label:"Italic",
+    onClick:()=>console.log("Italic clicked"),
+    src:Italic,
+    path:"/"
+  },
+  {
+    label:"Uppercase",
+    onClick:()=>console.log("Uppercase clicked"),
+    src:CaseUpperIcon,
+    path:"/"
+  },
+  {
+    label:"Audio",
+    onClick:()=>console.log("Audio clicked"),
+    src:AudioLines,
+    path:"/"
+  },
+  {
+    label:"Tags",
+    onClick:()=>console.log("Tags clicked"),
+    src:TagsIcon,
+    path:"/"
+  }
+]
+
+export default Buttons;
